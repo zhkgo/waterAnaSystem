31f226fe456730027da4cc746a7d0c56fb914a31
zhkgo <zhkgogogo@foxmail.com>
2018-11-29 22:20:23 +0800

    function is ok

--- nameList.py
+++ nameList.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import pandas as pd
+pathOfHotWords="keywords/hot.txt"
+pathOfReligions="keywords/citys.xls"
+pathOfHuman="keywords/human.txt"
+pathOfHuman1="keywords/human1.txt"
+
+def getTXT(filename):
+    with open(filename,"r") as f:
+        words=f.read().split()
+    return words
+def getHotWords():
+    return getTXT(pathOfHotWords)
+def getReligions():
+    return pd.read_excel(pathOfReligions)
+def getHuman():
+    return getTXT(pathOfHuman)
+def getHuman1():
+    return getTXT(pathOfHuman1)
