1f4053a6b6eccaccadb32c25e30df9b6198a6b88
zhkgo <zhkgogogo@foxmail.com>
2018-11-29 20:57:20 +0800

    more getData Function

--- getData.py
+++ getData.py
@@ -14,3 +14,21 @@ def load_tot_table():
     contents=cursor.fetchall()
     db.close()
     return contents
+def load_titles():
+    db=pymysql.connect(host='localhost',user='root',password='19971206zhk.',port=3306,charset='utf8')
+    cursor=db.cursor()
+    cursor.execute('use news')
+    cursor.execute('SELECT title,time FROM articlelist')
+    titles=cursor.fetchall()
+    db.close()
+    return titles
+
+def load_content():
+    db=pymysql.connect(host='localhost',user='root',password='19971206zhk.',port=3306,charset='utf8')
+    cursor=db.cursor()
+    cursor.execute('use news')
+    cursor.execute("SELECT content FROM articles")
+    contents=cursor.fetchall()
+    db.close()
+    return contents
+
