f132fa5fad6ef04db6f1a6ed222d9c62b1ed8546
zhkgo <zhkgogogo@foxmail.com>
2018-11-30 11:44:07 +0800

    data Wranging more

--- dataWranging.py
+++ dataWranging.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from getData import *
 from nameList import *
+import re
 #sql data to pandas DataFrame
 def toDataFrame(data,columns):
     test=list(zip(*data))
@@ -29,3 +30,13 @@ def getMapOfCity():
     cities=getPositionDF()
     mp={key:value for key,value in zip(cities['qu'],cities['city'])}
     return mp
+def my_cal(content,keys):
+    #count the appear times of key in content 
+    part_count={}
+    for item in keys:
+        part_count[item]=len(re.findall(item,content))
+    part_pairs=zip(part_count.values(),part_count.keys())
+    part_pairs=sorted(part_pairs)
+    labels=[x[1] for x in part_pairs]
+    values=[x[0] for x in part_pairs]
+    return labels,values
