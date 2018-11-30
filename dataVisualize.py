cdd2e60061c260476783def0c1fe4a9a404f0976
zhkgo <zhkgogogo@foxmail.com>
2018-11-30 16:36:39 +0800

    more functions

--- dataVisualize.py
+++ dataVisualize.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from dataWranging import *
-import jieba
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import seaborn as sns
-mpl.rcParams['figure.dpi']= 512
-sns.set(style="dark",color_codes=True)
-mpl.rcParams['font.sans-serif'] = 'FZSongKeBenXiuKaiT-R-GB'
 
+mpl.rcParams['figure.dpi']= 128
+sns.set(style="dark",color_codes=True)
+plt.style.use('seaborn-whitegrid')
+plt.rc('axes', unicode_minus=False)  
+mpl.rcParams['font.sans-serif'] = 'FZSongKeBenXiuKaiT-R-GB'
 stopwords=['水利','水利局' ,'水利工程','工程','建设','本站','发布','时间','负责人','一步','标准','重视']
 fontPath="fonts/fangzheng.ttf"
+
 def showCloud(words):
     mycloud=WordCloud(font_path=fontPath,width=800,height=400,stopwords=stopwords).generate(words)
     plt.imshow(mycloud)
@@ -34,16 +36,29 @@ def showBar(labels,values,top=10):
 
 def showCountry():
     content=getAllContents()
-    keys=
-    labels,value=my_cal(content,keys)
+    religions=getReligions()
+    keys=list(religions['下辖'])
+    labels,value=myCal(content,keys)
+    showBar(labels,value)
     plt.xlabel(u'县级市')
+    plt.ylabel(u'提及次数')
+    plt.title(u'提及次数排名前十')
+    plt.show()
+
+def showCitys():
+    content=getAllContents()
+    religions=getReligions()
+    keys=list(set(religions['行政']))
+    labels,value=myCal(content,keys)
+    showBar(labels,value)
+    plt.xlabel(u'地级市')
     plt.ylabel(u'提及次数')
     plt.title(u'提及次数排名前十')
     plt.show()
 
 def showPie(keys):
     content=getAllContents()
-    labels,values=my_cal(content,keys)
+    labels,values=myCal(content,keys)
     valuesum=sum(values)
     expl=[0.1 if x/valuesum >0.2 else 0 for x in values]
     plt.axis('equal')
@@ -54,4 +69,49 @@ def showHotPie():
     showPie(keys)
     plt.title('水利热词')
     
-    
+def showNewsYear(l,r):
+    r+=1
+    cntYears=getCntYears()
+    names=[str(x) for x in range(l,r)]
+    x=range(len(names))
+    y=[cntYears[i] for i in range(l,r)]
+    plt.plot(x,y,marker='*',mec='r',mfc='w',label='水利厅历年发文数量曲线图')
+    plt.legend()
+    plt.xticks(x,names,rotation=45)
+    plt.show()
+
+def showHotRankOfCity():
+    snsData=getRankData()
+    sns.boxplot(snsData.city,snsData.rankt)
+    plt.title('浙江各大城市热点指数排名')
+    plt.show()
+def showNumbersOfCity():
+    snsData=getRankData()
+    sns.boxplot(snsData.city,snsData.value)
+    plt.title('浙江各大城市热点指数排名')
+    plt.show()
+
+def showEmotionByQ():
+    dictM=getEmotionDictM()
+    dictM.plot(kind='barh',rot=0,color='r',title='各个季度情感指数')
+
+def showHotWords():
+    hotWords=getHotWordsData()
+    hotWords.plot(title='历年水利热词')
+    plt.xlabel('年份')
+    plt.ylabel('热力值')
+    plt.show()
+def showHotHuman():
+    hotHuman=getHotHumanData()
+    hotHuman.plot(title='历年水利人物图壹')
+    plt.xlabel('年份')
+    plt.ylabel('热力值')
+    #plt.savefig('human.jpg')
+    plt.show()
+def showHotHuman1():
+    hotHuman=getHotHumanData1()
+    hotHuman.plot(title='历年水利人物图贰')
+    plt.xlabel('年份')
+    plt.ylabel('热力值')
+    #plt.savefig('human.jpg')
+    plt.show()
--- dataWranging.py
+++ dataWranging.py
@@ -3,7 +3,11 @@
 from getData import *
 from nameList import *
 import re
+from snownlp import SnowNLP
+import jieba
 #sql data to pandas DataFrame
+stopwords=['',' ','的','工作','建设','\n','了','工程','水利','水利局' ,'水利工程','工程','本站','发布','时间','负责人','一步','标准','重视']
+
 def toDataFrame(data,columns):
     test=list(zip(*data))
     tempDict={}
@@ -30,7 +34,7 @@ def getMapOfCity():
     cities=getPositionDF()
     mp={key:value for key,value in zip(cities['qu'],cities['city'])}
     return mp
-def my_cal(content,keys):
+def myCal(content,keys):
     #count the appear times of key in content 
     part_count={}
     for item in keys:
@@ -40,3 +44,133 @@ def my_cal(content,keys):
     labels=[x[1] for x in part_pairs]
     values=[x[0] for x in part_pairs]
     return labels,values
+
+def sumValues(content,mp):
+    cities=list(set(mp.values()))
+    labels,values=myCal(content,cities)
+    result={}
+    for label,value in zip(labels,values):
+        result[label]=value
+    labels,values=myCal(content,list(mp.keys()))
+    for label,value in zip(labels,values):
+        result[mp.get(label,'_')]=result.get(mp.get(label,'_'),0)+value
+    return result
+
+
+def getCntYears():
+    df=getTitleDF()
+    cntYears={}
+    for i in range(len(df)):
+        cntYears[df['time'][i].year]=cntYears.get(df['time'][i].year,0)+1
+    return cntYears
+def getDataByDate():
+    columns=['title','content','time','origin']
+    data=loadTotTable()
+    data_init=toDataFrame(data,columns)
+    data_init['time']=pd.to_datetime(data_init['time'])
+    dataByDate=data_init.set_index('time')
+    return dataByDate
+
+def getQuaterlyData(bl=True):
+    quaterly=[]
+    contentsByQ=[]
+    dataByDate=getDataByDate()
+    for i in range(2013,2019):
+        for j in range(4):
+            quaterly.append(str(i)+'Q'+str(j+1))
+            ttemp=""
+            for k in range(j*3+1,j*3+4):
+                temp=dataByDate[str(i)+'-'+str(k)]
+                ttemp+=" ".join(temp['title'])
+                if(bl):
+                    ttemp+=" ".join(temp['content'])
+                if(bl):
+                    ttemp+=" ".join(temp['origin'])
+            contentsByQ.append(ttemp)
+    return quaterly,contentsByQ
+
+def getRankData():
+    snsData=pd.DataFrame()
+    quaterly,contentsByQ=getQuaterlyData()
+    mp=getMapOfCity()
+    for q,content in zip(quaterly,contentsByQ):
+        ret=sumValues(content,mp)
+        for key,value in zip(ret.keys(),ret.values()):
+            snsData=snsData.append(pd.Series({'q':q,'city':key,'value':value}),ignore_index=True)
+    snsData.city.name='城市'
+    snsData.q.name='季度'
+    snsData.value.name='新闻数'
+    snsData=snsData.sort_values(by='value',axis=0,ascending=False)
+    snsData['rankt']=list(range(1,len(snsData)+1))
+    snsData.rankt.name='热点指数排名'
+    snsData.city.name='城市'
+    snsData.q.name='季度'
+    snsData.value.name='热点指数'
+    snsData.rankt.name='热点指数排名'
+    return snsData
+def getEmotion(content):
+    s = SnowNLP(content)
+    i=0
+    tot=len(s.sentences)
+    print(tot)
+    ls=[]
+    for sentence in s.sentences:
+        ls.append(SnowNLP(sentence).sentiments)
+    return ls
+def getEmotionTot(contents):
+    result=[]
+    i=0
+    tot=len(contents)
+    for content in contents:
+        result.append(getEmotion(content))
+        print(str(i/tot*100)+'%')
+        i+=1
+    return result
+def trans(ls):
+    #trans ls to point if emotion>0.95 then add one
+    i=0
+    for item in ls:
+        if item>0.95:
+            i+=1
+    return i/len(ls)*100
+
+def getEmotionDictM():
+    quaterly,titlesByQ=getQuaterlyData(False)
+    #print(len(quaterly),len(titlesByQ))
+    result=getEmotionTot(titlesByQ)
+    k=[trans(item) for item in result]
+    dictM=pd.DataFrame({'季度':quaterly,'情感指数':k})
+    dictM=dictM.set_index('季度')
+    return dictM
+
+def is_ok(s):
+    return s not in stopwords
+def get_most(content):
+    wordlist=jieba.cut(content,cut_all=True)
+    wordlist=list(filter(is_ok, wordlist))
+    return pd.Series(wordlist).value_counts()
+def standard(pairs):
+    pairs=list(pairs)
+    tot=sum(pairs[1])
+    pairs[1]=[item/tot*100 for item in pairs[1]]
+    return pairs
+def getKeysData(keys):
+    contentsByQ=getQuaterlyData()[1]
+    ans=[standard(myCal(item,keys)) for item in contentsByQ]
+    columns=ans[0][0]
+    Dict={key:[] for key in columns}
+    for item in ans:
+        for key,value in zip(item[0],item[1]):
+            Dict[key].append(value)
+    dates=pd.date_range('2013-1-1','2018-11-1',freq='3M')
+    df=pd.DataFrame(Dict)
+    df.index=dates
+    return df
+
+def getHotWordsData():
+    return getKeysData(getHotWords())
+
+def getHotHumanData():
+    return getKeysData(getHuman())
+def getHotHumanData1():
+    return getKeysData(getHuman1())
